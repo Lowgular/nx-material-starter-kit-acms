b1614f6020e9e1aabfda48d5a05dc8bf87d68cf4
gradzio <gregradzio@gmail.com>
2022-09-06 08:44:46 +0200

    added form templates

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from "./material.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
