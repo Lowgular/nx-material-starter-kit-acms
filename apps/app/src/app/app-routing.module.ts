59e6028000295918d687c38e3e9cde812522ba55
gradzio <gregradzio@gmail.com>
2022-09-12 15:10:54 +0200

    preparing for multi route

---
+++ apps/app/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from "@angular/core";
+import {RouterModule} from "@angular/router";
+
+@NgModule({
+  imports: [RouterModule.forRoot([])],
+  providers: [],
+  declarations: [],
+  exports: []
+})
+export class AppRoutingModule {
+}
--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
+import {MaterialModule} from './material.module';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from "./material.module";
+import {AppRoutingModule} from "./app-routing.module";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, MaterialModule],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, MaterialModule, RouterModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
